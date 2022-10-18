0f9985115b991d600fe41a843aa2821e2ac6e4b7
StupidDeveloper05 <95141534+StupidDeveloper05@users.noreply.github.com>
2022-10-18 20:19:48 +0900

    g

--- myapp/config/settings.py
+++ myapp/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myapp.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myapp.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
